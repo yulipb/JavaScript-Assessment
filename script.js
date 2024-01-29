06b02a36a3b28fd3ea54628edfacf9bda3671676
Yulimar <yuli@yulimars-imac.lan>
2024-01-29 02:54:12 -0500

    committed

---
+++ script.js
@@ -0,0 +1,12 @@
+var firstName;
+var lastName;
+var age;
+
+firstName = "Yulimar";
+lastName = "Pagan-Betancourt";
+age = 17;
+
+alert("My name is " + firstName + " " + lastName + ".");
+alert("I am " + age + " years old.");
+
+console.log("I am actually having a lot of fun in this class. I find it to be very engaging! I won't lie it can be a bit difficut at times, but with a little trial and error, I know I can get it right.");
